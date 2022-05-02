3942f3c5392d79266e7e64142dea11e0a9524d6d
AminaGegi <77851660+AminaGegi@users.noreply.github.com>
2022-05-02 08:24:58 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      borderGrey: '#F2F1F4',
+      textGrey: '#A6A6A6',
+      black: '#000000',
+      btnGreen: "#0D9CED",
+      headerTextColor: "#1E2A3B",
+      loginColor: '#fafafa',
+      searchBorderColor: '#A1B0CC',
+      searchBGColor:'#F5F6F8',
+      loginBGColor: '#7C4E9A',
+      mainTextColor: '#323055',
+      loginGrey: '#757575',
+      listColor: '#584264',
+      treeCard: '#32293D',
+      talentTree: 'rgba(34, 34, 34, 0.8)',
+      whiteBG: 'rgba(255, 255, 255, 0.6)',
+      BGGrey: 'rgba(196, 196, 196, 0.39)',
+      textPurple: '#705391'
+    },
+  },
+  plugins: [],
+}
