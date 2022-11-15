cf7f20f65351821a94b95f3f20a66b3bfa6b14c1
AminaGegi <amina1309@mail.ru>
2022-11-15 14:41:08 +0300

    +

--- src/Components/Authorization/Authorization.js
+++ src/Components/Authorization/Authorization.js
@@ -1,12 +1,36 @@
 import React from "react";
-import Age from "./Age";
 import "../../style/authorization/Authorization.scss"
-import Gender from "./Gender";
-import {Link} from "react-router-dom";
 import StartHeader from "../StartHeader";
+import {useForm} from "react-hook-form";
+import axios from "axios";
+
+
 
 
 function Authorization (){
+    const URL_API = url-api;
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+        watch
+    } = useForm({mode:"onChange"})
+    const login = watch('login');
+    const email = watch('email');
+    const password = watch('password');
+    const cpassword = watch('cpassword');
+
+
+    const onSubmit = data => axios(
+        axios.post(URL_API, data)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    )
     return <main>
         <StartHeader/>
         <div className='authorization' style={{
@@ -19,19 +43,36 @@ function Authorization (){
                     <br/>знакомиться с твоими
                     <br/>единомышленниками
                 </div>
-                <div className='login__form'>
-                    <div className='login__form-name item'>
-                        <div>Имя ⃰</div>
-                        <input type='text' placeholder=''/>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='login__form'>
+                        <div className='login__form-name item'>
+                            <div>Логин ⃰</div>
+                            <input type='text' placeholder=''
+                                {...register('login',{required: true, maxLength: 15})}/>
+                            {errors.login && <i>Обязательное поле, не более 15 символов</i>}
+                        </div>
+                        <div className='login__form-name item'>
+                            <div>E-mail ⃰</div>
+                            <input type='email' placeholder=''
+                                   {...register('email',{required: true, pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"})}/>
+                            {errors.email && <i>Обязательное поле, не более 15 символов</i>}
+                        </div>
+                        <div className='login__form-name item'>
+                            <div>Пароль ⃰</div>
+                            <input type='password' placeholder=''
+                                   {...register('password',{required: true, maxLength: 15})}/>
+                            {errors.password && <i>Обязательное поле, не более 15 символов</i>}
+                        </div>
+                        <div className='login__form-name item'>
+                            <div>Повторите пароль ⃰</div>
+                            <input type='password' placeholder=''
+                                   {...register('cpassword',{required: true, validate: value => value === password})}/>
+                            {errors.cpassword && <i>Обязательное поле, пароли должны совпадать</i>}
+                        </div>
+
+                        <input type='submit' value='Зарегестрироваться' className='login__form-btn'/>
                     </div>
-                    <div className='login__form-surname item'>
-                        <div>Фамилия ⃰</div>
-                        <input type='text' placeholder=''/>
-                    </div>
-                    <Gender />
-                   <Age />
-                    <button className='login__form-btn'><Link to='../User'>Продолжить</Link></button>
-                </div>
+                </form>
             </div>
 
         </div>
